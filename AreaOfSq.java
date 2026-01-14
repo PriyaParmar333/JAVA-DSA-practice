26b041649e8f6f42dd537ce26eca5f3a3bcc1ec5
PriyaParmar333 <priyaparmar5526@gmail.com>
2026-01-14 20:59:50 +0530

    Add files via upload

---
+++ AreaOfSq.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class AreaOfSq
+
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+        //int x =sc.nextInt();
+       // int area=x*x;
+       // System.out.println(area);
+
+       float pencil=sc.nextFloat();
+       float pen=sc.nextFloat();
+       float eraser =sc.nextFloat();
+       float total=pencil+pen+eraser;
+       System.out.println(total);
+    }
+}
---
+++ JavaBasics.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class JavaBasics{
+    public static void main(String args[])
+    {
+        int age=16;
+        if(age>18)
+        {
+            System.out.println("ADULT..");
+        }
+        else
+        {
+            System.out.println("NOT ADULT...");
+        }
+    }
+    
+}
